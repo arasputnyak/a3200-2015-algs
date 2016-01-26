70c860a73fa08440774444838069360635d70023
Anastasiya <rasputnyak_n@inbox.ru>
2016-01-27 01:49:25 +0300

    Update lab11_tests.py

--- lab11/rasputnyak/lab11_tests.py
+++ lab11/rasputnyak/lab11_tests.py
@@ -2,20 +2,20 @@ import unittest
 import lab11
 
 class TestArea(unittest.TestCase):
-    def empty_lst(self):
+    def test_empty_lst(self):
         lst = []
         result = lab11.area(lst)
         expected = 0
         self.assertEqual(expected, result)
 
-    def one_elem_lst(self):
+    def test_one_elem_lst(self):
         lst = []
         lst.append(6)
         result = lab11.area(lst)
         expected = 0
         self.assertEqual(expected, result)
 
-    def example_area(self):
+    def test_example_area(self):
         lst = [2,5,1,2,3,4,7,7,6]
         result = lab11.area(lst)
         expected = 10
